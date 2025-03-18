5ac88f305307be1951fc149dd51948c71ebea157
Pierre Aillet <pierr611@gmail.com>
2025-03-18 16:45:32 +0100

    feat(swagger): setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription("Documentation de l'API NestJS")
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
