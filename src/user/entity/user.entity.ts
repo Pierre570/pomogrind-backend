601575bc76c1d63b7746ba05e60b70c23af7ebb2
Pierre Aillet <pierr611@gmail.com>
2025-04-07 11:10:14 +0200

    feat(user): add user entity

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 96,
+    nullable: false,
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    type: 'varchar',
+    length: 96,
+    nullable: false,
+  })
+  password: string;
+
+  @Column({
+    type: 'varchar',
+    length: 96,
+    nullable: false,
+  })
+  name: string;
+}
