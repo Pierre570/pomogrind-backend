ef3f2a02effcbeae64e9dac886cdfe867ed3e6c3
Pierre Aillet <pierr611@gmail.com>
2025-04-09 13:59:37 +0200

    feat(jwt): setup global environment for jwt tokens

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { HashingProvider } from './providers/hashing.provider';
+import { JwtTokensProvider } from './providers/jwt-tokens.provider';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, HashingProvider],
+  providers: [AuthService, HashingProvider, JwtTokensProvider],
   imports: [UsersModule],
 })
 export class AuthModule {}
